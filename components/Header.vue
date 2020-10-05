f557e7dd09c1d088f33b0f4e7d219cfc52b49d46
Leao04 <lordvergil12345@hotmail.com>
2020-10-05 17:45:02 -0300

    ...

--- components/Header.vue
+++ components/Header.vue
@@ -1,22 +1,13 @@
 <template>
   <div>
     <div>
-      <b-navbar type="dark" variant="dark">
+      <b-navbar type="danger" variant="dark">
         <b-navbar-nav>
-          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">Início</b-nav-item>
 
-          <!-- Navbar dropdowns -->
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
+          <b-nav-item href="#">Minhas publicações</b-nav-item>
 
-          <b-nav-item-dropdown text="User" right>
-            <b-dropdown-item href="#">Account</b-dropdown-item>
-            <b-dropdown-item href="#">Settings</b-dropdown-item>
-          </b-nav-item-dropdown>
+          <b-nav-item href="#">Linha do tempo</b-nav-item>
         </b-navbar-nav>
       </b-navbar>
     </div>
