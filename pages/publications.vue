d069a98b1160a510e44c17aeeb79ff9c7c6ea862
Leao04 <lordvergil12345@hotmail.com>
2020-10-07 18:34:43 -0300

    .

--- pages/publications.vue
+++ pages/publications.vue
@@ -3,7 +3,7 @@
     <div class="p">
       <h1>As melhores creepypasta do Brasil</h1>
     </div>
-    <img src="../assets/pentagrama.jpg" alt="imagem" />
+    <img src="../assets/img/pentagrama.jpg" alt="imagem" />
     <hr />
     <br />
     <div id="historia">
